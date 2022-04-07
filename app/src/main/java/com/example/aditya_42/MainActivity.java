21f0102d046a8cc9221e3818321f4930fc77039d
fyboc <adityammanjrekar@gmail.com>
2022-04-07 16:28:57 +0530

    practical

---
+++ app/src/main/java/com/example/aditya_42/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.aditya_42;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
